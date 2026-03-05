65c0c5aa30f7e789e55bd988af6a6e6ffc924d07
defi <defi>
2026-03-05 17:00:40 +0100

    include dotenv & remove log

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { app } from "./presentation/http/server";
 import { YieldSyncWorker } from "./infrastructure/workers/YieldSyncWorker";
 import { closeDB, testConnection } from "./db";
